5e0512cf136f50c26bc0664d7f7a77e7f0bdf96b
Ryan L <yamikaitou@gmail.com>
2018-02-05 23:37:47 +0000

    Test stuff cog

---
+++ yamik/yamik.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Yamik:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say(ctx.message.server.roles)
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))

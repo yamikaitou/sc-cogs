557bea162a1e4869b92254733ab68809b8173f79
Ryan L <yamikaitou@gmail.com>
2018-02-06 17:18:23 +0000

    get permissions?

--- yamik/yamik.py
+++ yamik/yamik.py
@@ -22,7 +22,7 @@ class Yamik:
         """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say(self._role_from_string(ctx.message.server, rolename))
+        await self.bot.say(self._role_from_string(ctx.message.server, rolename).permissions)
     
     def _role_from_string(self, server, rolename, roles=None):
         if roles is None:

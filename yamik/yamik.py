ac7dd0d7e3ffdad4d6cb0546eda7ae85c27b4318
Ryan L <yamikaitou@gmail.com>
2018-02-16 00:51:32 +0000

    permission dump

--- yamik/yamik.py
+++ yamik/yamik.py
@@ -23,12 +23,71 @@ class Yamik:
 
         #Your code will go here
         roles = ctx.message.server.roles
+        json = """{ "Count": {}, "Roles": [""".format(len(roles))
         for r in roles:
             if r.is_everyone:
-                await self.bot.say("{}|everyone|{}".format(r.position, r.permissions.value))
-            await self.bot.say("{}|{}|{}".format(r.position, r.name, r.permissions.value))
+                json += """{ "Name": "everyone", "Position": {}, Permissions": { 
+                    "CREATE_INSTANT_INVITE": {},"KICK_MEMBERS": {},
+                    "BAN_MEMBERS": {},"ADMINISTRATOR": {},
+                    "MANAGE_CHANNELS": {},"MANAGE_GUILD": {},
+                    "ADD_REACTIONS": {},"VIEW_AUDIT_LOG": {},
+                    "VIEW_CHANNEL": {},"SEND_MESSAGES": {},
+                    "SEND_TTS_MESSAGES": {},"MANAGE_MESSAGES": {},
+                    "EMBED_LINKS": {},"ATTACH_FILES": {},
+                    "READ_MESSAGE_HISTORY": {},"MENTION_EVERYONE": {},
+                    "USE_EXTERNAL_EMOJIS": {},"CONNECT": {},
+                    "SPEAK": {},"MUTE_MEMBERS": {},
+                    "DEAFEN_MEMBERS": {},"MOVE_MEMBERS": {},
+                    "USE_VAD": {},"CHANGE_NICKNAME": {},
+                    "MANAGE_NICKNAMES": {},"MANAGE_ROLES": {},
+                    "MANAGE_WEBHOOKS": {},"MANAGE_EMOJIS": {} } }""".format(r.position, 
+                    r.permissions.create_instant_invite,r.permissions.kick_members,
+                    r.permissions.ban_members,r.permissions.administrator,
+                    r.permissions.manage_channels,r.permissions.manage_server,
+                    r.permissions.add_reactions,r.permissions.view_audit_logs,
+                    r.permissions.read_messages,r.permissions.send_messages,
+                    r.permissions.send_tts_messages,r.permissions.manage_messages,
+                    r.permissions.embed_links,r.permissions.attach_files,
+                    r.permissions.read_message_history,r.permissions.mention_everyone,
+                    r.permissions.external_emojis,r.permissions.connect,
+                    r.permissions.speak,r.permissions.mute_members,
+                    r.permissions.deafen_members,r.permissions.move_members,
+                    r.permissions.use_voice_activation,r.permissions.change_nickname,
+                    r.permissions.manage_nicknames,r.permissions.manage_roles,
+                    r.permissions.manage_webhooks,r.permissions.manage_emojis)
+            else:
+                json += """",{ Name": "{}", "Position": {}, Permissions": { 
+                    "CREATE_INSTANT_INVITE": {},"KICK_MEMBERS": {},
+                    "BAN_MEMBERS": {},"ADMINISTRATOR": {},
+                    "MANAGE_CHANNELS": {},"MANAGE_GUILD": {},
+                    "ADD_REACTIONS": {},"VIEW_AUDIT_LOG": {},
+                    "VIEW_CHANNEL": {},"SEND_MESSAGES": {},
+                    "SEND_TTS_MESSAGES": {},"MANAGE_MESSAGES": {},
+                    "EMBED_LINKS": {},"ATTACH_FILES": {},
+                    "READ_MESSAGE_HISTORY": {},"MENTION_EVERYONE": {},
+                    "USE_EXTERNAL_EMOJIS": {},"CONNECT": {},
+                    "SPEAK": {},"MUTE_MEMBERS": {},
+                    "DEAFEN_MEMBERS": {},"MOVE_MEMBERS": {},
+                    "USE_VAD": {},"CHANGE_NICKNAME": {},
+                    "MANAGE_NICKNAMES": {},"MANAGE_ROLES": {},
+                    "MANAGE_WEBHOOKS": {},"MANAGE_EMOJIS": {} } }""".format(r.name, r.position, 
+                    r.permissions.create_instant_invite,r.permissions.kick_members,
+                    r.permissions.ban_members,r.permissions.administrator,
+                    r.permissions.manage_channels,r.permissions.manage_server,
+                    r.permissions.add_reactions,r.permissions.view_audit_logs,
+                    r.permissions.read_messages,r.permissions.send_messages,
+                    r.permissions.send_tts_messages,r.permissions.manage_messages,
+                    r.permissions.embed_links,r.permissions.attach_files,
+                    r.permissions.read_message_history,r.permissions.mention_everyone,
+                    r.permissions.external_emojis,r.permissions.connect,
+                    r.permissions.speak,r.permissions.mute_members,
+                    r.permissions.deafen_members,r.permissions.move_members,
+                    r.permissions.use_voice_activation,r.permissions.change_nickname,
+                    r.permissions.manage_nicknames,r.permissions.manage_roles,
+                    r.permissions.manage_webhooks,r.permissions.manage_emojis)
         
-        await self.bot.say("Done")
+        json += """] }"""
+        await self.bot.say(json)
     
     def _role_from_string(self, server, rolename, roles=None):
         if roles is None:

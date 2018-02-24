997d59111c381b6a51b2324df36e0a9bf7c23fd4
Ryan L <yamikaitou@gmail.com>
2018-02-24 01:57:58 +0000

    DynamoDB test

--- yamik/yamik.py
+++ yamik/yamik.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 import asyncio
 import logging
 import os
+import boto3
 
 
 log = logging.getLogger("red.admin")
@@ -20,50 +21,85 @@ class Yamik:
     @commands.command(pass_context=True)
     async def mycom(self, ctx, user: discord.Member=None):
         """This does stuff!"""
+        
+        db = boto3.resource("dynamodb")
+        table = db.Table("discord_groups")
 
         #Your code will go here
         roles = ctx.message.server.roles
-        json = '( "Count": {}, "Roles": ['.format(len(roles))
-        for r in roles:
-            if r.is_everyone:
-                json += '( "Name": "everyone", "Position": {}, Permissions": ( "CREATE_INSTANT_INVITE": {},"KICK_MEMBERS": {},"BAN_MEMBERS": {},"ADMINISTRATOR": {},"MANAGE_CHANNELS": {},"MANAGE_GUILD": {},"ADD_REACTIONS": {},"VIEW_AUDIT_LOG": {},"VIEW_CHANNEL": {},"SEND_MESSAGES": {},"SEND_TTS_MESSAGES": {},"MANAGE_MESSAGES": {},"EMBED_LINKS": {},"ATTACH_FILES": {},"READ_MESSAGE_HISTORY": {},"MENTION_EVERYONE": {},"USE_EXTERNAL_EMOJIS": {},"CONNECT": {},"SPEAK": {},"MUTE_MEMBERS": {},"DEAFEN_MEMBERS": {},"MOVE_MEMBERS": {},"USE_VAD": {},"CHANGE_NICKNAME": {},"MANAGE_NICKNAMES": {},"MANAGE_ROLES": {},"MANAGE_WEBHOOKS": {},"MANAGE_EMOJIS": {} ) )'.format(r.position, 
-                    r.permissions.create_instant_invite,r.permissions.kick_members,
-                    r.permissions.ban_members,r.permissions.administrator,
-                    r.permissions.manage_channels,r.permissions.manage_server,
-                    r.permissions.add_reactions,r.permissions.view_audit_logs,
-                    r.permissions.read_messages,r.permissions.send_messages,
-                    r.permissions.send_tts_messages,r.permissions.manage_messages,
-                    r.permissions.embed_links,r.permissions.attach_files,
-                    r.permissions.read_message_history,r.permissions.mention_everyone,
-                    r.permissions.external_emojis,r.permissions.connect,
-                    r.permissions.speak,r.permissions.mute_members,
-                    r.permissions.deafen_members,r.permissions.move_members,
-                    r.permissions.use_voice_activation,r.permissions.change_nickname,
-                    r.permissions.manage_nicknames,r.permissions.manage_roles,
-                    r.permissions.manage_webhooks,r.permissions.manage_emojis)
-            else:
-                json += ',( Name": "{}", "Position": {}, Permissions": ( "CREATE_INSTANT_INVITE": {},"KICK_MEMBERS": {},"BAN_MEMBERS": {},"ADMINISTRATOR": {},"MANAGE_CHANNELS": {},"MANAGE_GUILD": {},"ADD_REACTIONS": {},"VIEW_AUDIT_LOG": {},"VIEW_CHANNEL": {},"SEND_MESSAGES": {},"SEND_TTS_MESSAGES": {},"MANAGE_MESSAGES": {},"EMBED_LINKS": {},"ATTACH_FILES": {},"READ_MESSAGE_HISTORY": {},"MENTION_EVERYONE": {},"USE_EXTERNAL_EMOJIS": {},"CONNECT": {},"SPEAK": {},"MUTE_MEMBERS": {},"DEAFEN_MEMBERS": {},"MOVE_MEMBERS": {},"USE_VAD": {},"CHANGE_NICKNAME": {},"MANAGE_NICKNAMES": {},"MANAGE_ROLES": {},"MANAGE_WEBHOOKS": {},"MANAGE_EMOJIS": {} ) )'.format(r.name, r.position, 
-                    r.permissions.create_instant_invite,r.permissions.kick_members,
-                    r.permissions.ban_members,r.permissions.administrator,
-                    r.permissions.manage_channels,r.permissions.manage_server,
-                    r.permissions.add_reactions,r.permissions.view_audit_logs,
-                    r.permissions.read_messages,r.permissions.send_messages,
-                    r.permissions.send_tts_messages,r.permissions.manage_messages,
-                    r.permissions.embed_links,r.permissions.attach_files,
-                    r.permissions.read_message_history,r.permissions.mention_everyone,
-                    r.permissions.external_emojis,r.permissions.connect,
-                    r.permissions.speak,r.permissions.mute_members,
-                    r.permissions.deafen_members,r.permissions.move_members,
-                    r.permissions.use_voice_activation,r.permissions.change_nickname,
-                    r.permissions.manage_nicknames,r.permissions.manage_roles,
-                    r.permissions.manage_webhooks,r.permissions.manage_emojis)
+        with table.batch_writer() as batch:
+            for r in roles:
+                if r.is_everyone:
+                    table.put_item(
+                        Item={
+                            'Name':'everyone',
+                            'Position':r.position,
+                            "CREATE_INSTANT_INVITE":r.permissions.create_instant_invite,
+                            "KICK_MEMBERS":r.permissions.kick_members,
+                            "BAN_MEMBERS":r.permissions.ban_members,
+                            "ADMINISTRATOR":r.permissions.administrator,
+                            "MANAGE_CHANNELS":r.permissions.manage_channels,
+                            "MANAGE_GUILD":r.permissions.manage_server,
+                            "ADD_REACTIONS":r.permissions.add_reactions,
+                            "VIEW_AUDIT_LOG":r.permissions.view_audit_logs,
+                            "VIEW_CHANNEL":r.permissions.read_messages,
+                            "SEND_MESSAGES":r.permissions.send_messages,
+                            "SEND_TTS_MESSAGES":r.permissions.send_tts_messages,
+                            "MANAGE_MESSAGES":r.permissions.manage_messages,
+                            "EMBED_LINKS":r.permissions.embed_links,
+                            "ATTACH_FILES":r.permissions.attach_files,
+                            "READ_MESSAGE_HISTORY":r.permissions.read_message_history,
+                            "MENTION_EVERYONE":r.permissions.mention_everyone,
+                            "USE_EXTERNAL_EMOJIS":r.permissions.external_emojis,
+                            "CONNECT":r.permissions.connect,
+                            "SPEAK":r.permissions.speak,
+                            "MUTE_MEMBERS":r.permissions.mute_members,
+                            "DEAFEN_MEMBERS":r.permissions.deafen_members,
+                            "MOVE_MEMBERS":r.permissions.move_members,
+                            "USE_VAD":r.permissions.use_voice_activation,
+                            "CHANGE_NICKNAME":r.permissions.change_nickname,
+                            "MANAGE_NICKNAMES":r.permissions.manage_nicknames,
+                            "MANAGE_ROLES":r.permissions.manage_roles,
+                            "MANAGE_WEBHOOKS":r.permissions.manage_webhooks,
+                            "MANAGE_EMOJIS":r.permissions.manage_emojis
+                        })
+                else:
+                    table.put_item(
+                        Item={
+                            'Name':r.name,
+                            'Position':r.position,
+                            "CREATE_INSTANT_INVITE":r.permissions.create_instant_invite,
+                            "KICK_MEMBERS":r.permissions.kick_members,
+                            "BAN_MEMBERS":r.permissions.ban_members,
+                            "ADMINISTRATOR":r.permissions.administrator,
+                            "MANAGE_CHANNELS":r.permissions.manage_channels,
+                            "MANAGE_GUILD":r.permissions.manage_server,
+                            "ADD_REACTIONS":r.permissions.add_reactions,
+                            "VIEW_AUDIT_LOG":r.permissions.view_audit_logs,
+                            "VIEW_CHANNEL":r.permissions.read_messages,
+                            "SEND_MESSAGES":r.permissions.send_messages,
+                            "SEND_TTS_MESSAGES":r.permissions.send_tts_messages,
+                            "MANAGE_MESSAGES":r.permissions.manage_messages,
+                            "EMBED_LINKS":r.permissions.embed_links,
+                            "ATTACH_FILES":r.permissions.attach_files,
+                            "READ_MESSAGE_HISTORY":r.permissions.read_message_history,
+                            "MENTION_EVERYONE":r.permissions.mention_everyone,
+                            "USE_EXTERNAL_EMOJIS":r.permissions.external_emojis,
+                            "CONNECT":r.permissions.connect,
+                            "SPEAK":r.permissions.speak,
+                            "MUTE_MEMBERS":r.permissions.mute_members,
+                            "DEAFEN_MEMBERS":r.permissions.deafen_members,
+                            "MOVE_MEMBERS":r.permissions.move_members,
+                            "USE_VAD":r.permissions.use_voice_activation,
+                            "CHANGE_NICKNAME":r.permissions.change_nickname,
+                            "MANAGE_NICKNAMES":r.permissions.manage_nicknames,
+                            "MANAGE_ROLES":r.permissions.manage_roles,
+                            "MANAGE_WEBHOOKS":r.permissions.manage_webhooks,
+                            "MANAGE_EMOJIS":r.permissions.manage_emojis
+                        })
+                    
+            
         
-        json += '] )'
-        
-        json = json.replace("(", "{")
-        json = json.replace(")", "}")
-        json = json.replace("\\", "")
-        dataIO.save_json("data/yamik/perms.json", json)
         
         await self.bot.say("Done")
     

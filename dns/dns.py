f1bcf39292f9ecb6db471988ab93ddc7dfb5d829
Ryan L <yamikaitou@gmail.com>
2018-04-08 23:37:10 +0000

    switch to aiodns

--- dns/dns.py
+++ dns/dns.py
@@ -15,6 +15,8 @@ class dns:
         self.bot = bot
         self.settings = dataIO.load_json(os.path.join("data", "dns", "settings.json"))
         credentials = service_account.Credentials.from_service_account_file(os.path.join("data", "dns", "gcloud.json"))
+        loop = asyncio.get_event_loop()
+        self.resolver = aiodns.DNSResolver(loop=loop)
             
     @commands.group(pass_context=True, no_pm=True)
     @checks.is_owner()
@@ -57,15 +59,10 @@ class dns:
     async def nfo(self, ctx, ident, sub):
         """Create a DNS entry from an NFO name"""
         
-        ip_list = []
-        await ais = asyncio.get_event_loop().getaddrinfo("{}.game.nfoservers.com".format(ident),0)
-        for result in ais:
-          ip_list.append(result[-1][0])
-        ip_list = list(set(ip_list))
-        
+        ip = await resolver.query("{}.game.nfoservers.com".format(ident), 'A')
         client = dns.Client(project=self.settings["project"], credentials=credentials)
         zone = client.zone(self.settings['zone'], self.settings['domain'])
-        record_set = zone.resource_record_set('{}.{}.'.format(sub, self.settings['domain']), 'A', 60*60*2, [ip_list[0],])
+        record_set = zone.resource_record_set('{}.{}.'.format(sub, self.settings['domain']), 'A', 60*60*2, [ip,])
         changes = zone.changes()
         changes.create()
         while changes.status != 'done':

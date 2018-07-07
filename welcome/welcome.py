0657bb2707876f09309018e08973cd6dcf77724d
Ryan <yamikaitou@gmail.com>
2018-07-06 21:11:00 -0500

    Add files via upload
    welcome message

---
+++ welcome/welcome.py
@@ -0,0 +1,15 @@
+import discord
+
+
+class Welcome:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_member_join(self, member):
+        embed = discord.Embed(colour=discord.Colour(0x91022d),
+                              description="Welcome to the SuperCentral Gaming Community Discord\n\nPlease read #welcome to learn more about our community\nGive yourself some roles in #verification\nRequest answers to questions related to our community in #support\nReport rule violators on our servers in #reports\nSocialize with other members of the community in #general\nWe hope you enjoy your stay here\n\nOh, and don't forget to introduce yourself in #introduction")
+
+        await bot.say(content="Greetings {} :wave:".format(member.mention), embed=embed)
+
+def setup(bot):
+    bot.add_cog(Welcome(bot))

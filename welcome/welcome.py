a3271def532b3480ec981491127e5369fdbd25db
Ryan <yamikaitou@gmail.com>
2018-07-06 21:27:44 -0500

    Update welcome.py
    forgot self

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -9,7 +9,7 @@ class Welcome:
         embed = discord.Embed(colour=discord.Colour(0x91022d),
                               description="Welcome to the SuperCentral Gaming Community Discord\n\nPlease read #welcome to learn more about our community\nGive yourself some roles in #verification\nRequest answers to questions related to our community in #support\nReport rule violators on our servers in #reports\nSocialize with other members of the community in #general\nWe hope you enjoy your stay here\n\nOh, and don't forget to introduce yourself in #introduction")
 
-        await bot.send_message(member.server.get_channel("350900912887431178"), content="Greetings {} :wave:".format(member.mention), embed=embed)
+        await self.bot.send_message(member.server.get_channel("350900912887431178"), content="Greetings {} :wave:".format(member.mention), embed=embed)
 
 def setup(bot):
     bot.add_cog(Welcome(bot))

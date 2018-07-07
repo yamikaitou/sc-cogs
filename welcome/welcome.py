5e7d856e4b84b49c58c9325eb07543418512a254
Ryan <yamikaitou@gmail.com>
2018-07-06 21:36:29 -0500

    Update welcome.py
    actually mention the channels

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -8,7 +8,7 @@ class Welcome:
     async def on_member_join(self, member):
         serv = member.server
         embed = discord.Embed(colour=discord.Colour(0x91022d),
-                              description="Welcome to the SuperCentral Gaming Community Discord\n\nPlease read {} to learn more about our community\nGive yourself some roles in {}\nRequest answers to questions related to our community in {}\nReport rule violators on our servers in {}\nSocialize with other members of the community in {}\nWe hope you enjoy your stay here\n\nOh, and don't forget to introduce yourself in {}".format(serv.get_channel("192701949575954435"),serv.get_channel("451147557607833600"),serv.get_channel("318204253791584268"),serv.get_channel("383405657161990145"),serv.get_channel("192153663555371008"),serv.get_channel("350900912887431178")))
+                              description="Welcome to the SuperCentral Gaming Community Discord\n\nPlease read {} to learn more about our community\nGive yourself some roles in {}\nRequest answers to questions related to our community in {}\nReport rule violators on our servers in {}\nSocialize with other members of the community in {}\nWe hope you enjoy your stay here\n\nOh, and don't forget to introduce yourself in {}".format(serv.get_channel("192701949575954435").mention,serv.get_channel("451147557607833600").mention,serv.get_channel("318204253791584268").mention,serv.get_channel("383405657161990145").mention,serv.get_channel("192153663555371008").mention,serv.get_channel("350900912887431178").mention))
 
         await self.bot.send_message(serv.get_channel("350900912887431178"), content="Greetings {} :wave:".format(member.mention), embed=embed)
 
